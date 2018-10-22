64c8bb2b86596a69a983e8b49f576c6d69b425c9
Dustin Delatore <dustin@delatore.me>
2018-10-22 13:34:50 -0400

    add analytics"

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import PrismicVue from 'prismic-vue'
 import VueClipboard from 'vue-clipboard2'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(PrismicVue, {
   endpoint: window.prismic.endpoint
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-33579395-7'
 })
 
 Vue.use(VueClipboard)
